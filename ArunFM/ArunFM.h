d9d5bfa4b02804710be3d60f6c8acacd5f4df6c5
ArunPIos <arunpandian2041@gmail.com>
2024-03-17 14:44:07 +0530

    Added source file

---
+++ ArunFM/ArunFM.h
@@ -0,0 +1,18 @@
+//
+//  ArunFM.h
+//  ArunFM
+//
+//  Created by Arun_Pandian on 15/03/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ArunFM.
+FOUNDATION_EXPORT double ArunFMVersionNumber;
+
+//! Project version string for ArunFM.
+FOUNDATION_EXPORT const unsigned char ArunFMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ArunFM/PublicHeader.h>
+
+
---
+++ ArunFM/ArunStruct.swift
@@ -0,0 +1,14 @@
+//
+//  ArunStruct.swift
+//  ArunFM
+//
+//  Created by Arun_Pandian on 15/03/24.
+//
+
+import Foundation
+
+public struct ArunStruct {
+    public static func callThisMethod() {
+        print("something")
+    }
+}
